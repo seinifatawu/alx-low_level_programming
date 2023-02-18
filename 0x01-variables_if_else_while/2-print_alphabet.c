c481fc8f69dd0b00eb53cb2df325621ad2d68d72
Abdul-Fatawu <seini.coe@gmail.com>
2023-02-18 12:25:56 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry of main code
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
