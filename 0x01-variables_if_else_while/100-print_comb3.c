8eceee8ffa1249c473e2b0db96ef7f2b8acc1ee3
Abdul-Fatawu <seini.coe@gmail.com>
2023-02-18 22:29:37 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,20 +6,21 @@
  */
 int main(void)
 {
-	int a, b;
+int a, b;
 
-	for (a = 0; a < 10; a++)
-	{
-	for (b = a + 1; b < 10; b++)
-	{
-		putchar('0' + a);
-		putchar('0' + b);
-		if (a + b != 17)
-		{putchar(',');
-		}
-		putchar(' ');
-	}
-	}
-	putchar('\n');
-	return (0);
+for (a = 0; a < 10; a++)
+{
+for (b = a + 1; b < 10; b++)
+{
+putchar('0' + a);
+putchar('0' + b);
+if (a + b != 17)
+{
+putchar(',');
+}
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
