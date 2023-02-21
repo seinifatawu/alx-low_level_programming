5ae8ea6fc96d6a3ce9c786f667d7b79a96fe4c33
Abdul-Fatawu <seini.coe@gmail.com>
2023-02-21 05:24:54 -0800

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - entry of main code
+ * Return: 0
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
